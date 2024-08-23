9475209b9d1fbeeadbf14053ef988bd2aa3fde9b
Cam <cameronmanzo111@gmail.com>
2024-08-23 18:24:32 -0400

    login logout database

--- microblog.py
+++ microblog.py
@@ -1,1 +1,8 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor # $ flask shell: this makes it so dont have to import every time
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
